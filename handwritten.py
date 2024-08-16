5f06f1f0c897abc8b716391336bd6cd5532a6db6
sainadha-reddy3 <158275434+sainadha-reddy3@users.noreply.github.com>
2024-08-16 10:39:21 +0530

    Add files via upload

---
+++ handwritten.py
@@ -0,0 +1,5 @@
+import pywhatkit as pw
+txt="""My name is Sainadha Reddy. Iam from Hyderabad.
+ Iam a computer science Student. Studying in Amity university"""
+pw.text_to_handwriting(txt,"demo1.png",[0,0,138])
+print(" END ") 
